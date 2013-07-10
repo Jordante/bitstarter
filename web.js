c5f25ad3cb7c5560c2f0895fed02d238867cdd22
Jordan Meyer <jordanbmeyer@gmail.com>
2013-07-10 05:54:52 +0000

    lets try this again

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var buff1 = new Buffer(fs.readFileSync('/bitstarter/index.html','utf8');
-    response.send(buff1.toSTring());
+    var buff1 = new Buffer();
+    response.send(buff1.toSTring(fs.readFileSync("index.html",'utf-8')));
 });
 
 var port = process.env.PORT || 5000;
